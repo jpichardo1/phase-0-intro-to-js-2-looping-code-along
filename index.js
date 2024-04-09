395cada9c913b7eb1b0c06d71374b5509881f554
Jessica Pichardo <jess.pichardo18@gmail.com>
2024-04-08 23:53:02 -0700

    last

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// Code your solution in this file
+function writeCards(names) {
+    const thankYouMessages = [];
+  
+    names.forEach(name => {
+      const message = `Thank you, ${name}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message);
+    });
+  
+    return thankYouMessages;
+  }
+  
+  const names = ["Guadalupe", "Ollie", "Aki"];
+  const messages = writeCards(names);
+  console.log(messages);
+
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  countDown(5);
